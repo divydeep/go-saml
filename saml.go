dcdcffe50cb459af2a8f183e7e3a3b2bd725c1b4
Joseph Taylor <josephtaylor.mn@gmail.com>
2017-12-20 12:05:43 -0600

    Update saml.go

--- saml.go
+++ saml.go
@@ -13,6 +13,7 @@ type ServiceProviderSettings struct {
 	IDPPublicCertPath           string
 	AssertionConsumerServiceURL string
 	SPSignRequest               bool
+	ValidateSignarture          bool
 
 	hasInit       bool
 	publicCert    string
